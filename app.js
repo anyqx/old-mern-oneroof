02fab1dce2d73318031d4832a7a79613045736a2
JosephASandoval <josephasandoval17@gmail.com>
2021-02-22 10:51:49 -0800

    updated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+
 
 mongoose
     .connect(db, {
@@ -12,10 +16,13 @@ mongoose
     .then(() => console.log('Connected to MongoDB successfully'))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req,res) => {res.send('Hello World')});
 
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
-
 
